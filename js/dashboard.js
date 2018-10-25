92f50345d90cb20f2bf83655f165466aff6806ac
Ankit Karnany <ankit@greylabs.org>
2018-10-25 18:50:37 +0530

    displaying gifs from api

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,27 @@
 var gifyApp = angular.module('gifyApp');
 
-gifyApp.controller('dashboardCtrl', ['$scope', '$location', 'gify', function ($scope, $location, gify) {
+gifyApp.controller('dashboardCtrl', ['$scope', 'gify', '$timeout', function ($scope, gify, $timeout) {
 
   $scope.logout = () => {
     $location.path('/');
   }
 
+  $scope.gifs = [];
+  let offset = 0;
+
+  function fetchGifs() {
+    gify.search({
+      offset: offset
+    }).then(
+      function (res) {
+        $scope.gifs = $scope.gifs.concat(res.data.data);
+        offset = res.data.pagination.offset + res.data.pagination.count;
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  }
+  fetchGifs();
+  $timeout(fetchGifs, 3000);
 }]);
--- js/index.js
+++ js/index.js
@@ -38,12 +38,31 @@ gifyApp.service('gify', ['$http', function ($http) {
   this.search = function (req) {
     return $http({
       method: 'GET',
-      url: 'https://api.giphy.com/v1/gifs/search?api_key=F4H0W1vLeAa94BX196mYLu8USqgnz0zy&q=cheeseburgers&limit=10',
+      url: 'https://api.giphy.com/v1/gifs/search?api_key=F4H0W1vLeAa94BX196mYLu8USqgnz0zy&q=cheeseburgers&limit=4&offset=' + req.offset,
       headers: {
         'Content-Type': 'application/json'
-      },
-      data: req
+      }
     });
   }
 
 }]);
+
+gifyApp.directive('gif', function () {
+  return {
+    restrict: 'A',
+    scope: {
+      gif: "="
+    },
+    link: function (scope, element, attrs) {
+      console.log(element);
+      element.on('mouseenter', function () {
+        attrs.$set('ngSrc', scope.gif.images.fixed_width.url);
+        element.addClass('hovered');
+      });
+      element.on('mouseleave', function () {
+        attrs.$set('ngSrc', scope.gif.images['480w_still'].url);
+        element.removeClass('hovered');
+      });
+    }
+  };
+});
